662c5b8925edf413248e3c30d8a815d89aa0b474
Fer <fgarcia@mail.wm.edu>
2018-12-28 13:33:08 -0500

    new windows

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,25 @@
-$(".btn")
+// 1. Create a function that initiates game upon the click of the "start" button. 
+$(button).on("click", function () {
+    // if user clicks "start" begin game.
+    // launchGame();
+
+});
+
+function launchGame(){
+    // initTimer();
+};
+
+var clockRunning= false;
+var time= 60;
+
+
+
+function initTimer() {
+
+
+
+}
+
+// 2. Create the function "Launch Game."
+    // >create a timer to time questions.
+    // >create a form that contains trivia questions.
