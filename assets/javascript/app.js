5e17f89561312a35ce1760b23874073b9f574dc0
Fer <fgarcia@mail.wm.edu>
2018-12-30 11:54:26 -0500

    v.1 using multiple windows, not splash screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,44 @@
 // 1. Create a function that initiates game upon the click of the "start" button. 
-$(button).on("click", function () {
+$("#startBtn").on("click", function () {
     // if user clicks "start" begin game.
-    // launchGame();
+    
+    initTimer();
 
 });
 
-function launchGame(){
-    // initTimer();
-};
 
-var clockRunning= false;
-var time= 60;
 
+// var clockRunning= false;
+var time= 120;
+var intervalId;
 
 
 function initTimer() {
-
-
-
+    
+    
+    intervalId= setInterval(decrement, 1000);
+    
 }
 
-// 2. Create the function "Launch Game."
-    // >create a timer to time questions.
-    // >create a form that contains trivia questions.
+function decrement() {
+   
+    $("#counter").html(time + "seconds");
+   
+    time--;
+
+    if (time === 0) {
+        stop();
+        window.open("./feedback.html")
+        // endGame();
+    }
+}
+
+function stop() {
+    clearInterval(intervalId);
+}
+
+initTimer();
+
+var correctAnswers=0;
+var incorrectAnswers=0;
+var unanswered=0;
